20b26a8734065f4fed4c313f60dd798645108057
Faraz <fzv@fzv.ca>
2015-03-21 08:48:48 +1300

    Added a better notification file

---
+++ samples/cliptnotify.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+export XMSG_TIMEOUT=6
+
+export PHASE=$1
+export MINUTES=$2
+export TTYNAME=$3
+
+if [ $# -ne 3 ]; then
+	echo "Invalid number of arguments"
+	exit 1;
+fi
+
+case $PHASE in
+	"b") 
+		export TXT="Take a short break for $2 minutes."
+		;;
+	"w")
+		export TXT="Focus on work for $2 minutes, now!"
+		;;
+	"l")
+		export TXT="Take a bit longer break $2 minutes, enjoy!"
+		;;
+	"q")
+		export TXT="Time to go home :)"
+		;;
+	*)
+		#invalid phase indicator
+		exit 1
+		;;
+esac
+
+export XMESSAGE=`which xmessage`
+
+if [ -n "$XMESSAGE" -a -n "$DISPLAY" ]; then
+	$XMESSAGE -center -timeout $XMSG_TIMEOUT "$TXT"
+else
+	mesg y
+	
+	TXT="----------\n$TXT\n----------";
+	echo -e $TXT | write `whoami` $3  
+fi
+
--- samples/pomscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "$0:$1:$2:$3" >> /tmp/pomo.out
-export user=`whoami`
-
-echo "phase $1 for $2 minute starts now" | write $user $3
-
